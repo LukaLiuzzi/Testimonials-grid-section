06af99c6c24bca11937b95779dc3cc81e772f750
Luka Liuzzi <lukaliuzzidev@gmail.com>
2022-01-02 20:56:29 -0300

    Solution on mobile and 1920x1080px

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        violet: "hsl(263, 55%, 52%)",
+        gray: "hsl(217, 19%, 35%)",
+        blackish: "hsl(219, 29%, 14%)",
+      },
+      fontFamily: {
+        sans: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
